bc36535704bbe2d0bb63ab0239a91330b27e9581
Evan <evan@heyevan.com>
2024-03-05 11:04:39 -0500

    imports link

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import useEffect from 'react';
+import Link from 'next/link';
 
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
